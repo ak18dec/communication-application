35886f6904cb0e8c2c6903cb45df6d7d11f43423
Ankit Kumar <ankitdayal1992@gmail.com>
2021-09-26 13:23:54 +0530

    code added for project

---
+++ main.js
@@ -0,0 +1,18 @@
+var users = [
+    {
+        username: "Test User",
+        email: "textuser@gmail.com"
+    },
+    {
+        username: "Anne Hunter",
+        email: "anne.hunter@mail.com"
+    },
+    {
+        username: "Jale Boser",
+        email: "jale@yahoo.com"
+    }
+]
+
+function deleteUser(user) {
+    alert(JSON.stringify(user));
+}
